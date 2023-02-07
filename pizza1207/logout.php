970fca286b7af95e9284861c36e3f2eaee31482f
jegyvasarlas <88721560+jegyvasarlas@users.noreply.github.com>
2023-02-07 14:55:37 +0100

    l

---
+++ pizza1207/logout.php
@@ -0,0 +1,20 @@
+<?php
+include 'functions.php';
+session_unset(); // Kiüríti a munkamenetet
+session_destroy(); // Eldobja a munkamenetet
+include 'menu.php';
+?>
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP és MySQL összekapcsolása</title>
+</head>
+<body>
+    <?php echo $menu; ?>
+    <h2>Kijelentkezés sikeres!</h2>
+</body>
+</html>
+
